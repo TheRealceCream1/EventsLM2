6cc1c7702b7c5dfffec8a7b62c638fcd7c4e3eb0
TheRealceCream1 <s017769@students.lmsd.org>
2023-02-24 10:04:17 -0500

    Initial Commit

---
+++ EventLM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EventLM
+//
+//  Created by Owen Bress (student LM) on 2/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventLM/EventLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventLMApp.swift
+//  EventLM
+//
+//  Created by Owen Bress (student LM) on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct EventLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
